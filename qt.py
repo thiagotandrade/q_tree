4db5bfbedd62ddad5a523f93c9bf8e6087da9028
thiagotandrade <thiagotales@gmail.com>
2019-04-17 11:25:44 -0300

    Query Tree Algorithm

--- qt.py
+++ qt.py
@@ -1,9 +1,9 @@
+# coding=utf-8
 '''
     TODO:
-
     Ver pra que serve a memória M;
     Ver o que fazer no caso de sucesso;
-    Criar variáveis para guardar o que aconteceu com a query (s, c, v);
+    Criar variáveis para guardar o que aconteceu com a query (s, c, e);
     Ver como armazenar os dados de cada simulação.
 '''
 
@@ -26,9 +26,15 @@ class simulationParameters():
 start = time.time()
 params = simulationParameters()
 tag_count = params.MIN_TAGS
+
+# List of tags IDs
 tags = []
+# Stack of queries
 Q = ['']
+# Memory (stores successfuly read tags ids)
 M = []
+# Amount of successes, collisions and empty for each simulation
+success = collision = empty = 0
 
 
 
@@ -38,26 +44,40 @@ while tag_count <= params.MAX_TAGS:
     for _ in range(tag_count):
         tags.append(bin(random.getrandbits(params.TAG_LENGTH))[2:])
 
-    for simulation in range(params.SIMULATIONS):
+    for simulation in range(1,params.SIMULATIONS+1):
+        
+        print("Simulation #: {}".format(simulation))
+
         # Query Tree algorithm
-        matched_indices = []
         while Q:
             current_query = Q.pop()
 
             # Find tags indexes that contain current prefix
             matched_indices = [i for i, tag in enumerate(tags) if tag.startswith(current_query)]
             
-            if len(matched_indices) == 1:
-                # Sucess
-                ''' Talvez retirar a tag da lista de tags? '''
+            # Success
+            if len(matched_indices) == 1: 
+                M.append(tags[matched_indices[0]])
+                success += 1
+                #print('Success', success)
 
-            else if len(matched_indices) > 1:
-                # Collision
+            # Collision
+            elif len(matched_indices) > 1:
                 Q.append(current_query + '0')
                 Q.append(current_query + '1')
+                collision += 1
+                #print('Collision', collision)
 
-            else:
-                #Empty, does nothing
+            # Empty            
+            else: 
+                empty += 1
+                #print('Empty', empty)
+
+        
+        Q = ['']
+        M = []
+        success = collision = empty = 0
+        
     
     tags = []
     tag_count += params.STEP
