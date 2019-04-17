562848fb3bfeb5b1dfc1774dbab0fc30a1a3024d
thiagotandrade <thiagotales@gmail.com>
2019-04-17 09:36:25 -0300

    Código para realização da query

--- qt.py
+++ qt.py
@@ -1,3 +1,12 @@
+'''
+    TODO:
+
+    Ver pra que serve a memória M;
+    Ver o que fazer no caso de sucesso;
+    Criar variáveis para guardar o que aconteceu com a query (s, c, v);
+    Ver como armazenar os dados de cada simulação.
+'''
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,7 +30,8 @@ tags = []
 Q = ['']
 M = []
 
-# Generate the number of tags, with steps of 100. Then simulate 100 times for each amount of tags
+
+
 while tag_count <= params.MAX_TAGS:
     
     # Generate random tags IDs
@@ -30,20 +40,24 @@ while tag_count <= params.MAX_TAGS:
 
     for simulation in range(params.SIMULATIONS):
         # Query Tree algorithm
+        matched_indices = []
         while Q:
             current_query = Q.pop()
-            '''
-                Ver como singularizar a tag;
-                Ver pra que serve a memória M;
-                Ver como armazenar os dados de cada simulação; 
-                Achar indices das tags que contem o prefixo (Lembrar de inicializar indices): 
-                    indices = [i for i, tag in enumerate(my_list) if tag.startswith(current_query)]
-                Se houver colisão, fazer:
-                    Q.append(current_query + '0')
-                    Q.append(current_query + '1')
-            '''
 
+            # Find tags indexes that contain current prefix
+            matched_indices = [i for i, tag in enumerate(tags) if tag.startswith(current_query)]
+            
+            if len(matched_indices) == 1:
+                # Sucess
+                ''' Talvez retirar a tag da lista de tags? '''
 
+            else if len(matched_indices) > 1:
+                # Collision
+                Q.append(current_query + '0')
+                Q.append(current_query + '1')
+
+            else:
+                #Empty, does nothing
     
     tags = []
     tag_count += params.STEP
