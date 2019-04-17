be691db7f594aff2dd6ab33760600f92fc0920cc
thiagotandrade <thiagotales@gmail.com>
2019-04-17 08:26:22 -0300

    inicialização da query e definição do laço da QT

--- qt.py
+++ qt.py
@@ -5,7 +5,7 @@ import random
 import time
 
 # Simulation parameters
-class params(): 
+class simulationParameters(): 
     SIMULATIONS = 100
     MAX_TAGS = 1000
     MIN_TAGS = 100
@@ -15,16 +15,13 @@ class params():
 
 # Initializing variables
 start = time.time()
-params = params()
+params = simulationParameters()
 tag_count = params.MIN_TAGS
 tags = []
-'''
-COMO INICIALIZAR A PILHA Q?
-'''
-Q = []
+Q = ['']
 M = []
 
-# Loop to generate the number of tags, with steps of 100. Then simulate for each amount
+# Generate the number of tags, with steps of 100. Then simulate 100 times for each amount of tags
 while tag_count <= params.MAX_TAGS:
     
     # Generate random tags IDs
@@ -33,12 +30,18 @@ while tag_count <= params.MAX_TAGS:
 
     for simulation in range(params.SIMULATIONS):
         # Query Tree algorithm
-        # current_query = Q.pop()
-        while true:
+        while Q:
+            current_query = Q.pop()
+            '''
+                Ver como singularizar a tag;
+                Ver pra que serve a memória M;
+                Ver como armazenar os dados de cada simulação; 
+            '''
+
 
     
     tags = []
-    tag_count = tag_count + params.STEP
+    tag_count += params.STEP
 
 
 end = time.time()
