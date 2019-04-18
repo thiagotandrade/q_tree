5d7b08e3a5cae5f63d4c7050e2563e45286c2878
Thiago Andrade <ttca@cin.ufpe.br>
2019-04-17 23:31:02 -0300

    Generating csv with simulation data

--- qt.py
+++ qt.py
@@ -12,20 +12,23 @@ import matplotlib.pyplot as plt
 import random
 import time
 
-# Values Store Classes
-class SimulationParameters(): 
+
+class SimulationParameters: 
     SIMULATIONS = 100
     MAX_TAGS = 1000
     MIN_TAGS = 100
     STEP = 100
     TAG_LENGTH = 64
 
-class Metrics():
-    collision = 0
-    empty = 0
-    sent_bits = 0
-    execution = 0
-    tag_count = 0
+# Lists to store all simulations data
+class Metrics:
+    def __init__(self):
+        self.collision = []
+        self.empty = []
+        self.sent_bits = []
+        self.execution = []
+        self.tag_count = []
+        self.simulation = []
 
 def QT(tags):
     collision = empty = sent_bits = 0
@@ -92,57 +95,90 @@ def QTsc(tags):
     
     return collision, empty, sent_bits
 
+def saveToDf(df, tag_count, simulation, collision, empty, sent_bits, execution):
+    df['TAG_COUNT'] = tag_count
+    df['SIMULATION_NUMBER'] = simulation
+    df['COLISION_SLOTS'] = collision
+    df['EMPTY_SLOTS'] = empty
+    df['SENT_BITS'] = sent_bits
+    df['SIMULATION_TIME'] = execution
+
+    return df
+
 def main():
     # Initializing variables
     params = SimulationParameters()
     tag_count = params.MIN_TAGS
-
+    collision = empty = sent_bits = 0
     # List of tags IDs
     tags = []
-
     # Objects to store simulation metrics
     qt = Metrics()
     qtsc = Metrics()
-    
-    # Initialize dataframes for plotting
-    column_names = ['SIMULATION_NUMBER', 'TAG_COUNT', 'COLISION_SLOTS', 'EMPTY_SLOTS', 'SENT_BITS', 'SIMULATION_TIME']
-    qt_df = pd.DataFrame(columns=column_names)
-    qtsc_df = pd.DataFrame(columns=column_names)
 
     while tag_count <= params.MAX_TAGS:
-        print("Tag count: {}".format(tag_count))
+        print("\nTag count: {}".format(tag_count))
+        print("Simulation #: ", end='')
 
         for simulation in range(1,params.SIMULATIONS+1): 
-            print("Simulation #: {}".format(simulation))
+            print("{} ".format(simulation), end='')
+
+            #Store current simulation number and tags amount for later plotting
+            qt.tag_count.append(tag_count)
+            qtsc.tag_count.append(tag_count)
+            qt.simulation.append(simulation)
+            qtsc.simulation.append(simulation)
 
             # Generate random tags IDs
             for _ in range(tag_count):
                 tags.append(bin(random.getrandbits(params.TAG_LENGTH))[2:].zfill(params.TAG_LENGTH))
             
-            # Query Tree Simulation
+            '''
+                QT Algorithm
+            '''           
             start = time.time()
-            qt.collision, qt.empty, qt.sent_bits = QT(tags)
+            
+            # Query Tree Simulation
+            collision, empty, sent_bits = QT(tags)
+            
+            qt.collision.append(collision)
+            qt.empty.append(empty)
+            qt.sent_bits.append(sent_bits)
             end = time.time()
             # Calculate QT execution time in seconds
-            qt.execution = end - start
+            qt.execution.append(end - start)
 
-            # Query Tree Shortcut Simulation
+            '''
+                QT-sc Algorithm
+            '''
             start = time.time()
-            qtsc.collision, qtsc.empty, qtsc.sent_bits = QTsc(tags)
+            
+            # Query Tree Shortcut Simulation            
+            collision, empty, sent_bits = QTsc(tags)
+            
+            qtsc.collision.append(collision)
+            qtsc.empty.append(empty)
+            qtsc.sent_bits.append(sent_bits)
             end = time.time()
             # Calculate QTsc execution time in seconds
-            qtsc.execution = end - start
+            qtsc.execution.append(end - start)
 
-            qt_df = qt_df.append(pd.Series([simulation, tag_count, qt.collision, qt.empty, qt.sent_bits, qt.execution], index=column_names), ignore_index=True)
-            qtsc_df = qtsc_df.append(pd.Series([simulation, tag_count, qtsc.collision, qtsc.empty, qtsc.sent_bits, qtsc.execution], index=column_names), ignore_index=True)
+            # Clear tags list for next simulation
+            tags = []
 
-
-        tags = []
+        
         tag_count += params.STEP
     
 
+    # Save results into Dataframes for plotting
+    column_names = ['SIMULATION_NUMBER', 'TAG_COUNT', 'COLISION_SLOTS', 'EMPTY_SLOTS', 'SENT_BITS', 'SIMULATION_TIME']
+    qt_df = pd.DataFrame(columns=column_names)
+    qtsc_df = pd.DataFrame(columns=column_names)
+    qt_df = saveToDf(qt_df, qt.tag_count, qt.simulation, qt.collision, qt.empty, qt.sent_bits, qt.execution)
+    qtsc_df = saveToDf(qtsc_df, qtsc.tag_count, qtsc.simulation, qtsc.collision, qtsc.empty, qtsc.sent_bits, qtsc.execution)
+
     qt_df.to_csv('qt.csv', index=False)
-    qtsc_df.to_csv('qt_sc_df.csv', index=False)
+    qtsc_df.to_csv('qtsc.csv', index=False)
 
 
 
